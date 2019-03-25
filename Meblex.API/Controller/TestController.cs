ea2f16a7009df2d9c24be81b5c665bc2a1473e2e
Mikart <mikart143@gmail.com>
2019-03-25 15:57:54 +0100

    Initial Commit

---
+++ Meblex.API/Controller/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Meblex.API.Controller
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController :ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            return StatusCode(200, new Test(){Name = "dupa"});
+
+        }
+    }
+    public class Test
+    {
+        public string Name { get; set; }
+    }
+}
